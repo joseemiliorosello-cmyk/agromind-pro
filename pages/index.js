3d4453fa55e294c5c7a28f30458784ce5471016f
José Emilio Rosello Brajovich <rosello.jose@inta.gob.ar>
2026-03-30 11:18:28 -0300

    fix: GraficoBalance TrayectoriaVaquillona en index

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ import { calcCerebro, buildPromptFull, SYS_FULL,
 import { usePersistencia, PanelHistorial } from "../lib/persistencia";
 import { Pill, Alerta, smf2, DistCC, Input, LoadingPanel,
          MetricCard, SelectF, Slider, Toggle, SuplSelector } from "../components/ui";
-import { DashboardEstablecimiento, ScoreRadar } from "../components/dashboard";
+import { DashboardEstablecimiento, ScoreRadar, GraficoBalance, TrayectoriaVaquillona } from "../components/dashboard";
 import { getPasoRenders } from "../components/pasos";
 
 const MSGS = [
